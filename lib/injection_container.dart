5ff34d8d3e7741c1e13d741743214df2cebdbefe
Shubhangi-H98 <hatwarshubhangi77@gmail.com>
2026-03-28 17:18:02 +0530

    chore(di): register CatalogCubit in service locator

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,11 +2,20 @@ import 'package:get_it/get_it.dart';
 import 'core/network/api_client.dart';
 import 'features/catalog/domain/repositories/product_repository.dart';
 import 'features/catalog/data/repositories/product_repository_impl.dart';
+import 'features/catalog/presentation/cubit/catalog_cubit.dart';
 
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
+
+  // Cubit: Register as a Factory so a new instance is created every time
+  sl.registerFactory(() => CatalogCubit(sl()));
+
+  // Repository: Register as a LazySingleton (one instance for the whole app)
+  sl.registerLazySingleton<ProductRepository>(
+        () => ProductRepositoryImpl(sl()),
+  );
   // Core: Registering ApiClient as a Singleton.
   sl.registerLazySingleton(() => ApiClient());
 
